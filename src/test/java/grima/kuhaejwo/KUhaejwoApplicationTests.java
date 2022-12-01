2df4d1b3770b8200ff69112c9dac7f6e8ecb2156
ehdtndla123 <ehdtndla123@naver.com>
2022-12-01 21:40:21 +0900

    Start Project

---
+++ src/test/java/grima/kuhaejwo/KUhaejwoApplicationTests.java
@@ -0,0 +1,13 @@
+package grima.kuhaejwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KUhaejwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
