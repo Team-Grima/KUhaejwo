8a774de76adcd14422397e959abd8fb2764a6b6d
ehdtndla123 <ehdtndla123@naver.com>
2022-12-01 23:32:03 +0900

    Base Response

---
+++ src/main/java/grima/kuhaejwo/config/model/response/CommonResponse.java
@@ -0,0 +1,15 @@
+package grima.kuhaejwo.config.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CommonResponse {
+
+    SUCCESS(200,"성공 하였습니다."),
+    FAIL(500,"실패 하였습니다.");
+
+    private int code;
+    private String msg;
+}
---
+++ src/main/java/grima/kuhaejwo/config/model/response/CommonResult.java
@@ -0,0 +1,10 @@
+package grima.kuhaejwo.config.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonResult {
+
+}
