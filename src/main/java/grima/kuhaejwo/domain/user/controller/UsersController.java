90805d87cf15e65c9e587ceff5664010e4125181
ehdtndla123 <ehdtndla123@naver.com>
2022-12-23 20:29:59 +0900

    Info CRUD

--- src/main/java/grima/kuhaejwo/domain/user/controller/UsersController.java
+++ src/main/java/grima/kuhaejwo/domain/user/controller/UsersController.java
@@ -1,9 +1,14 @@
 package grima.kuhaejwo.domain.user.controller;
 
+import grima.kuhaejwo.config.model.response.SingleResult;
+import grima.kuhaejwo.config.model.service.ResponseService;
 import grima.kuhaejwo.domain.user.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UsersController {
     private final UsersService usersService;
+    private final ResponseService responseService;
+
+    @Operation(description = "유저 기본 정보를 생성합니다.")
+    @PostMapping("/info")
+    public SingleResult<Long> createInfo(
+            @Header(name = "X-AUTH-TOKEN",
+                    description = "로그인 성공 후 AccessToken",
+                    required = true
+    ))
+    {
+
+    }
 
 }
