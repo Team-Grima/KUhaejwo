f42707aa62e0d1b456e1d87b87f750809917f00a
ehdtndla123 <ehdtndla123@naver.com>
2022-12-23 20:31:51 +0900

    Service CRUD

--- src/main/java/grima/kuhaejwo/domain/user/service/UsersService.java
+++ src/main/java/grima/kuhaejwo/domain/user/service/UsersService.java
@@ -1,7 +1,13 @@
 package grima.kuhaejwo.domain.user.service;
 
+import grima.kuhaejwo.config.security.JwtProvider;
 import grima.kuhaejwo.domain.user.dao.UsersRepository;
+import grima.kuhaejwo.domain.user.domain.BasicInfo;
+import grima.kuhaejwo.domain.user.domain.Users;
+import grima.kuhaejwo.domain.user.dto.UserBasicInfoRequest;
+import grima.kuhaejwo.domain.user.dto.UserBasicInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +18,34 @@ public class UsersService {
 
     private final UsersRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    //반환 값 무엇으로 할까요
+    @Transactional
+    public Long createInfo(String token, UserBasicInfoRequest userBasicInfoRequest) {
+        Long userLongPk = Long.parseLong(jwtProvider.getUserPk(token));
+        Users user = userRepository.findById(userLongPk).orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
+        BasicInfo basicInfo = userBasicInfoRequest.toEntity();
+        user.setBasicInfo(basicInfo);
+        return userLongPk;
+    }
+
+    @Transactional
+    public UserBasicInfoResponse getInfo(String token) {
+        Long userLongPk = Long.parseLong(jwtProvider.getUserPk(token));
+        Users user = userRepository.findById(userLongPk).orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
+        return new UserBasicInfoResponse(user.getBasicInfo());
+    }
+    //업데이트 부분 어떻게 할까요
+    @Transactional
+    public Long updateInfo(String token, UserBasicInfoRequest userBasicInfoRequest) {
+        Long userLongPk = Long.parseLong(jwtProvider.getUserPk(token));
+        Users user = userRepository.findById(userLongPk).orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
+        BasicInfo basicInfo = userBasicInfoRequest.toEntity();
+        user.setBasicInfo(basicInfo);
+        return userLongPk;
+    }
+
 
 
 }
