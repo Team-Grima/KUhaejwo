6fc3910b621ebd948e4d269e4ff5a619b5578853
ehdtndla123 <ehdtndla123@naver.com>
2022-12-26 17:22:01 +0900

    JPA Auditing

--- src/main/java/grima/kuhaejwo/KUhaejwoApplication.java
+++ src/main/java/grima/kuhaejwo/KUhaejwoApplication.java
@@ -2,7 +2,9 @@ package grima.kuhaejwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class KUhaejwoApplication {
 
